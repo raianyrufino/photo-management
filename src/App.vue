66846908fddbf8e319b419a30890999240f3b6ef
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-10-12 23:03:23 -0300

    changes in structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
+  <div class="corpo">
 
-  <div>
-    <h1>{{ titulo }}</h1>
+    <h1 class="titulo">{{ titulo }}</h1>
 
-    <ul>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
 
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-corpo">
+
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+
+          </div><!-- fim painel-corpo -->
+        </div><!-- fim painel -->
+
       </li>
-
     </ul>
 
   </div>
-
 </template>
 
 <script>
@@ -36,4 +42,49 @@ export default {
 </script>
 
 <style>
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
