44af5f57b9022687d1166d259243f7ac81ef2a49
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-10-13 08:16:30 -0300

    shared component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
+
 <template>
   <div class="corpo">
-
-    <h1 class="titulo">{{ titulo }}</h1>
+    <h1 class="centralizado">{{ titulo }}</h1>
 
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
 
-        <div class="painel">
-          <h2 class="painel-titulo">{{foto.titulo}}</h2>
-          <div class="painel-corpo">
-
+        <meu-painel :titulo="foto.titulo">
             <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-
-          </div><!-- fim painel-corpo -->
-        </div><!-- fim painel -->
+        </meu-painel>
 
       </li>
     </ul>
@@ -22,35 +17,43 @@
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
 
- data () {
-    return {
-      titulo: "pic",
-      fotos: []
+  components: {
+    'meu-painel' : Painel
+  },
 
+  data() {
+
+    return {
+
+      titulo: 'Alurapic', 
+      fotos: []
     }
   },
+
   created() {
 
     this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
   }
-
 }
+
 </script>
 
 <style>
-
-  .titulo {
-    text-align: center;
-  }
-
   .corpo {
     font-family: Helvetica, sans-serif;
-    margin: 0 auto;
     width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+
+    text-align: center;
   }
 
   .lista-fotos {
@@ -58,33 +61,12 @@ export default {
   }
 
   .lista-fotos .lista-fotos-item {
+
     display: inline-block;
   }
 
   .imagem-responsiva {
+
     width: 100%;
-  }
-
-  /* estilo do painel */ 
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
